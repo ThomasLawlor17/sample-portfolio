39688c244c9d22587f78e80c2e9930f8e7eb53fd
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2023-07-16 14:12:12 -0400

    v1 projects section done

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -2,20 +2,134 @@ import { styled } from "styled-components"
 
 
 const StyledSection = styled.section`
+width: 77%;
+max-width: 1110px;
+margin: auto;
+
+header {
+    ${({theme}) => theme.mixins.flexBetween};
+    margin-bottom: 80px;
+    button {
+        ${({theme}) => theme.mixins.button};
+    }
+}
+
+div.grid {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(auto, 545px));
+    justify-content: space-evenly;
+    row-gap: 69px;
+    column-gap: 30px;
+}
 
 
+@media (max-width: 769px) {
+    width: 92.457737%;
+    max-width: 713px;
+
+    div.grid {
+        grid-template-columns: repeat(2, minmax(auto, 345px));
+        column-gap: 20px;
+    }
+}
+@media (max-width: 677px) {
+    div.grid {
+        display: flex;
+        flex-direction: column;
+    }
+}
+@media (max-width: 414px) {
+    width: calc(100% - 30px);
+}
 `
 
 const StyledProject = styled.div`
+max-width: 545px;
+position: relative;
+.p-img {
+    width: 100%;
+    aspect-ratio: 5.45 / 4;
+    max-height: 400px;
+    background-size: cover;
 
+    &::after {
+        content: '';
+        display: block;
+        width; 100%;
+        height: 100%;
+        background: #000;
+        opacity: 0;
+        transition: all 0.3s linear;
+    }
+
+    &:hover {
+        &::after {
+            opacity: 0.75;
+        }
+        a {
+            opacity: 1;
+            z-index: 2;
+        }
+    }
+}
+h3 {
+    margin: 20px 0 7px 0;
+    text-transform: uppercase;
+}
+ul {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 18px;
+    li {
+        text-transform: uppercase;
+        color: var(--light-grey);
+        font-weight: 500;
+        font-size: 18px;
+        line-height: 28px;
+    }
+}
+a {
+    ${({theme}) => theme.mixins.button}
+    @media (min-width: 770px) {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        opacity: 0;
+        
+        &.p-link {
+            top: 28.3%;
+        }
+        &.c-link {
+            top: 46%;
+        }
+    }
+}
+@media (max-width: 769px) {
+
+    div.links {
+        padding-top: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 32px;
+        a {
+            width: fit-content;
+        }
+    }
+}
 
 `
 
 export default function Projects() {
+
+    const width = window.innerWidth
+
+
     const projects = [
         {
             name: 'Design Portfolio',
-            tech: ['html', 'css']
+            tech: ['html', 'css'],
         },
         {
             name: 'E-Learning Landing Page',
@@ -46,16 +160,29 @@ export default function Projects() {
             <h1>Projects</h1>
             <button>Contact me</button>
         </header>
-        <div>
+        <div className="grid">
             {projects.map((p, i) => (
                 <StyledProject key={i}>
-                    <img src={process.env.PUBLIC_URL + `/assets/images/thumbnail-project-${i}-large.webp`} alt={p.name} />
+                    <div className="p-img" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/thumbnail-project-${i + 1}-large.webp)`}}>
+                    {width >= 770 ? 
+                    <>
+                    <a className="p-link" href="/">view project</a>
+                    <a className="c-link" href="/">view code</a>
+                    </>
+                    :''}
+                    </div>
                     <h3>{p.name}</h3>
                     <ul>
                         {p.tech.map((t,i) => (
                             <li key={i}>{t}</li>
-                        ) )}
+                            ) )}
                     </ul>
+                    {width < 770 ? 
+                    <div className="links">
+                    <a className="p-link" href="/">view project</a>
+                    <a className="c-link" href="/">view code</a>
+                    </div>
+                    :''}
                 </StyledProject>
             ))}
         </div>
